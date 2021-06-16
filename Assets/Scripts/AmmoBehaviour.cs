3e68c897d9336c0a237f9f54f8cf8572914acd3c
felix <fbquinn7@gmail.com>
2021-06-16 14:01:56 +1000

    Project Initialisation
    Game files added to repo

---
+++ Assets/Scripts/AmmoBehaviour.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoBehaviour : MonoBehaviour
+{
+
+    [SerializeField]
+    private int ammoClip;
+    public int clipSize;
+    public int ammoMax;
+    public GameObject bulletPrefab;
+    public GameObject bulletSpawnPoint;
+    private float reloadTime;
+    private bool stopReload;
+
+    public int ammoPickUpSmall = 10;
+    public int ammoPickUpBig = 25;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletSpawnPoint = transform.GetChild(0).gameObject;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (reloadTime <= 0)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Mouse0) && ammoMax >= 0 && ammoClip != 0)
+            {
+                ammoClip--;
+                GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, bulletSpawnPoint.transform.rotation);
+            }
+
+            if(ammoMax == 0)
+            {
+                stopReload = true;
+            }
+            else
+            {
+                stopReload = false;
+            }
+
+            if (Input.GetKeyDown("r") && stopReload == false)
+            {
+                ammoMax = ammoMax - clipSize;
+                ammoClip = clipSize;
+                reloadTime = 2;
+                //Debug.Log("Yay i just reloaded");
+            }
+        }
+        reloadTime -= Time.deltaTime; 
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("ammoBig"))
+        {
+            ammoMax = ammoMax + ammoPickUpBig;
+            Destroy(other.gameObject);
+        }
+    }
+
+}
